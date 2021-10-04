23747586739249b404978f3a5ff44b3cf2f2d5bd
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2021-10-04 19:49:27 +0900

    テストPy

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
-import numpy as np
-import yeelight
+import requests
+import datetime
 
 
-bulb = yeelight.Bulb("192.168.11.4")
-bulb.turn_on()
-bulb.set_rgb(255, 255, 255)
-bulb.set_brightness(10)
+def geturls(sum,mode):
+    today = datetime.date.today().strftime("%Y/%m/%d")
+    time = datetime.datetime.now().strftime("%H:%M:%S")
+    get = 'https://script.google.com/macros/s/AKfycbz9PoBi_Bs_P6aiI3D_cR3O1fU6JhGUu5J1ewF0R-VEwDNF65tm-7HJUze6qJOQoLMP/exec?p1='+str(today)+'&p2='+str(time)+'&p3='+str(mode)+'&p4='+str(sum)
+    requests.get(get)
+
+geturls(sum,"service")
