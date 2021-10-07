64c68123576502d6bdbe11c17581cf702217afc5
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2021-10-07 15:54:36 +0900

    メモ追加

--- test.py
+++ test.py
@@ -8,4 +8,9 @@ def geturls(sum,mode):
     get = 'https://script.google.com/macros/s/AKfycbz9PoBi_Bs_P6aiI3D_cR3O1fU6JhGUu5J1ewF0R-VEwDNF65tm-7HJUze6qJOQoLMP/exec?p1='+str(today)+'&p2='+str(time)+'&p3='+str(mode)+'&p4='+str(sum)
     requests.get(get)
 
-geturls(sum,"service")
+geturls(sum,"service")#service 運用中,Restart 再起動中,stop 停止中,other メンテナンス中
+
+#pip install numpy
+#pip install opencv-contrib-python
+#pip install yeelight
+#pip install requests
